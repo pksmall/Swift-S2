afba4a0d5e567e78ba19e38bb86c27028c947821
Pavel Korzhenko <admin@mac-admin.local>
2017-12-22 18:11:50 +0200

    Initial Commit

---
+++ Weather20171220/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather20171220
+//
+//  Created by Pavel Korzhenko on 12/22/17.
+//  Copyright © 2017 pavelkorzhenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
